7a557461f5400c64b196ec096fa3575e623fd0c5
krabappel2548 <krabappel2548@gmail.com>
2014-11-21 12:01:26 +0100

    Add rhine modem files

--- rhine/rhine-partial.mk
+++ rhine/rhine-partial.mk
@@ -66,6 +66,27 @@ PRODUCT_PACKAGES += \
 	leia_pm4_470 \
 	mba_b00 \
 	mba_mdt \
+	modem_b00 \
+	modem_b01 \
+	modem_b02 \
+	modem_b03 \
+	modem_b04 \
+	modem_b05 \
+	modem_b08 \
+	modem_b11 \
+	modem_b13 \
+	modem_b14 \
+	modem_b15 \
+	modem_b16 \
+	modem_b17 \
+	modem_b18 \
+	modem_b19 \
+	modem_b20 \
+	modem_b21 \
+	modem_b22 \
+	modem_b25 \
+	modem_b26 \
+	modem_mdt \
 	touch_module_id_0x45 \
 	touch_module_id_0x48 \
 	touch_module_id_0x49 \
